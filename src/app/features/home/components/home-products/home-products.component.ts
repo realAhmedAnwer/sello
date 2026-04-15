18bff673bf737af96c6825007d865007dd0dcfa6
Ahmed Anwer <104807894+realAhmedAnwer@users.noreply.github.com>
2026-04-15 11:34:48 +0200

    Implement home product listing and create Product interface

--- src/app/features/home/components/home-products/home-products.component.ts
+++ src/app/features/home/components/home-products/home-products.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Product } from '@shared/models/product.interface';
+import { ProductsService } from '@shared/services/products.service';
 
 @Component({
   selector: 'app-home-products',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './home-products.component.html',
   styleUrl: './home-products.component.css',
 })
-export class HomeProductsComponent {}
+export class HomeProductsComponent implements OnInit {
+  private readonly _productsService = inject(ProductsService);
+
+  public productList = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts(): void {
+    this._productsService.getAllProducts().subscribe({
+      next: (res) => {
+        console.log(res.data)
+        this.productList.set(res.data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
