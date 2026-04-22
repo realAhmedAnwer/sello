6ae68a1436a571c0cdfd61ae294bcf97f41d5bbe
Ahmed Anwer <104807894+realAhmedAnwer@users.noreply.github.com>
2026-04-22 06:45:21 +0200

    Add auth guard to protect routes and ensure user authentication

---
+++ src/app/core/auth/guards/auth-guard.ts
@@ -0,0 +1,17 @@
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const platformId = inject(PLATFORM_ID);
+  const router = inject(Router);
+
+  if (isPlatformBrowser(platformId)) {
+    if (localStorage.getItem('accessToken')) {
+      return true;
+    } else {
+      return router.parseUrl('/login');
+    }
+  }
+  return true;
+};
